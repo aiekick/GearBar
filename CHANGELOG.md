b695f8abaf3a8b546a0e17e87a66c2f84103d479
aiekick <aiekick@funparadigm.com>
2024-12-08 17:25:38 +0100

    -

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,6 @@ All notable changes to the "gearbar" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-## [Unreleased]
+## [0.01]
 
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,58 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gearbar" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('gearbar.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from GearBar!');
-	});
-
-	context.subscriptions.push(disposable);
+interface ItemConfig {
+  setting: string;
+  label: string;
+  icon: string;
+  tooltip: string;
 }
 
-// This method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+  statusBarItem.text = "$(gear)";
+  statusBarItem.tooltip = "Click to toggle features";
+  statusBarItem.command = "gearbar.showMenu";
+  statusBarItem.show();
+  context.subscriptions.push(statusBarItem);
+
+  const disposable = vscode.commands.registerCommand("gearbar.showMenu", async () => {
+    const itemsConfig: ItemConfig[] = loadItemsConfig();
+    if (!itemsConfig || itemsConfig.length === 0) {
+      vscode.window.showErrorMessage("No configuration items found in settings. Please configure 'gearbar.items' in your user/workspace settings.");
+      return;
+    }
+
+    const config = vscode.workspace.getConfiguration();
+    const quickPickItems: vscode.QuickPickItem[] = itemsConfig.map(item => {
+      const currentValue = config.get<boolean>(item.setting, false);
+      return {
+        label: `${item.icon} ${item.label}`,
+        description: currentValue ? "$(check)" : "",
+        detail: item.tooltip
+      };
+    });
+
+    const selection = await vscode.window.showQuickPick(quickPickItems, {
+      placeHolder: "Select a feature to toggle"
+    });
+
+    if (selection) {
+      const selectedItem = itemsConfig.find(item => `${item.icon} ${item.label}` === selection.label);
+      if (selectedItem) {
+        const currentValue = config.get<boolean>(selectedItem.setting, false);
+        await config.update(selectedItem.setting, !currentValue, vscode.ConfigurationTarget.Global);
+        vscode.window.showInformationMessage(`${selectedItem.label} is now ${!currentValue ? 'enabled' : 'disabled'}.`);
+      }
+    }
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+function loadItemsConfig(): ItemConfig[] {
+  const config = vscode.workspace.getConfiguration();
+  const items = config.get<ItemConfig[]>("gearbar.items", []);
+  return items;
+}
+
 export function deactivate() {}
